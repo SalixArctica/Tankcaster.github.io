b66055346ab913c6cfabcdf9ce675516389c6879
Tankcaster <33927097+Tankcaster@users.noreply.github.com>
2018-05-03 17:46:22 -0500

    Added jquery animation to link butto

--- animationScript.js
+++ animationScript.js
@@ -2,8 +2,26 @@
 
 $(document).ready(function()
 {
-	$('#linkButtons').on('mouseenter', function()
+	//if they click the "links" link
+	$('#links').click(function()
 	{
-		$(event.currentTarget).css('height', '100px');
+		//shake the link images
+		shake($("#linkButtons").find("img"));
 	});
 });
+
+function shake(thing)
+{
+	//grow
+	thing.animate(
+	{
+		height: "55px",
+		width: "55px"
+	}, 500);
+	//shrink
+	thing.animate(
+	{
+		height: "50px",
+		width: "50px"
+	}, 500);
+}
