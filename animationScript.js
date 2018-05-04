06ebd67c980074f2ea74c722681e66fbf1d017e3
Tankcaster <33927097+Tankcaster@users.noreply.github.com>
2018-05-04 14:41:58 -0500

    added link button click animation and changed font

--- animationScript.js
+++ animationScript.js
@@ -8,8 +8,12 @@ $(document).ready(function()
 		//shake the link images
 		shake($("#linkButtons").find("img"));
 	});
+	
+	$('#linkButtons').find("img").click(function()
+	{
+		buttonClick($(event.currentTarget));
+	});
 });
-
 function shake(thing)
 {
 	//grow
@@ -24,4 +28,18 @@ function shake(thing)
 		height: "50px",
 		width: "50px"
 	}, 500);
+}
+
+function buttonClick(button)
+{
+	button.animate(
+	{
+		height: "45px",
+		width: "45px"
+	}, 110);
+	button.animate(
+	{
+		height: "50px",
+		width: "50px"
+	}, 110);
 }
